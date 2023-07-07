d7c74d01495beba260b3e23148ecbea349d7b18a
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2023-07-07 08:53:00 +0700

    member structure

--- src/entity/member.entity.ts
+++ src/entity/member.entity.ts
@@ -3,11 +3,19 @@ import mongoose, { Schema } from "mongoose";
 export interface IMember {
   code: string;
   name: string;
+  borrowedBooks: unknown;
 }
 
 const memberSchema = new Schema<IMember>({
   code: { type: String, required: true, unique: true },
   name: { type: String, required: true },
+  borrowedBooks: {
+    type: {
+      type: [Schema.Types.ObjectId],
+      ref: "Book",
+      default: [],
+    },
+  },
 });
 
 const Member = mongoose.model("Member", memberSchema, "member");
